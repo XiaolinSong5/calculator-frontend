ad5c23c019114c7cf9f7ee83725436b52fc60f12
Xiaolin <xiaolin.song@benerail.com>
2022-12-28 16:30:12 +0100

    Calculation type

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -17,12 +17,18 @@ export class CalculatorService {
   private subject = new Subject<any>();
 // private apiUrl = "http://localhost:5000/calculations";
   private apiUrl = "http://localhost:8080/calculations";
+  private componentsUrl = "http://localhost:8080/components";
   private addUrl = "http://localhost:8080/create";
   private deleteUrl = "http://localhost:8080/delete";
+  //private dialgueUrl = "http://localhost:8001/dialogue-engine/dialogues/2f50050c-cc25-4e9f-ade5-635b2b284b30";
   constructor(private http: HttpClient) { }
   addCalculation(calculation: Calculation): Observable<Calculation> {
     console.log("Service add calculation first: " + calculation.firstNumber  + "json: "+ JSON.stringify(calculation));
     console.log("addUrl" + this.addUrl);
+    // this.http.get<any>('http://localhost:8001/dialogue-engine/dialogues/2f50050c-cc25-4e9f-ade5-635b2b284b30').subscribe(data => {
+    //   console.log("dialogue: " + data);
+    // })
+
     let a = this.http.post("http://localhost:8080/delete",calculation, httpOptions ).subscribe((result) => (
       this.display = result.toString()));
     this.subject.next(this.display)
@@ -38,7 +44,7 @@ export class CalculatorService {
     // );
   }
   getCalculations(): Observable<Calculation[]> {
-    return this.http.get<Calculation[]>(this.apiUrl);
+    return this.http.get<Calculation[]>(this.componentsUrl);
   }
   getResult(): Observable<any> {
     return this.subject.asObservable();
